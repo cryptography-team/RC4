889dad7cfe19e4e1b75030e43356d3d2dfc73808
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-05-07 14:19:59 +0200

    changed project structure

---
+++ src/RC4Muns.cpp
@@ -0,0 +1,9 @@
+#include "../include/RC4.h"
+BBSG ::BBSG(int length) {}
+
+void BBSG ::generateTwoPrimeNumbers() {}
+//////////////////////////
+void RC4 ::damageKeyStream() {}
+string RC4 ::encrypt(const string &plain) {}
+string RC4 ::decrypt(const string &cipher) {}
+RC4 ::~RC4();
