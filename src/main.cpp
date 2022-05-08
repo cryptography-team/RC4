39f1482c135afa3480b714613d5f3c488def3e55
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-05-09 01:04:35 +0200

    fixed a small bug and added the ability to store the last generated cipher text

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 int main() {
   RC4 streamCipher(128);
+  string cipher;
   while (true) {
     cout << "Choose one of the following options:\n";
     cout << "\t1- Encrypt a plain text\n";
-    cout << "\t2- Decrypt a cipher text\n";
+    cout << "\t2- Decrypt the last cipher text\n";
     cout << "\t3- Set a random key\n";
     cout << "\t4- Set a custom key\n";
     cout << "\t*- Exit the program\n";
@@ -21,29 +22,27 @@ int main() {
       cout << "Enter your plain text:\n\t" << flush;
       string plain;
       getline(cin, plain);
-      cout << "The corresponding cipher text is:\n\t"
-           << streamCipher.encrypt(plain) << endl;
+      cipher = streamCipher.encrypt(plain);
+      cout << "The corresponding cipher text is:\n\t" << cipher << endl;
     } else if (option == 2) {
-      cout << "Enter your cipher text:\n\t" << flush;
-      string cipher;
-      getline(cin, cipher);
       cout << "The corresponding plain text is:\n\t"
            << streamCipher.decrypt(cipher) << endl;
     } else if (option == 3) {
-      cout << "Enter your key:\n\t" << flush;
-      string key;
-      getline(cin, key);
-      streamCipher.setKey(key);
-      cout << "The key has been set\n";
-    } else if (option == 4) {
       cout << "Enter its length: " << flush;
       int len;
       cin >> len;
       cin.ignore();
       cout << "The following key has been generated and set:\n\t"
            << streamCipher.generateRandomKey(len) << endl;
+    } else if (option == 4) {
+      cout << "Enter your key:\n\t" << flush;
+      string key;
+      getline(cin, key);
+      streamCipher.setKey(key);
+      cout << "The key has been set\n";
     } else
       break;
+    cout << endl;
 #ifdef __unix__
     system("read -p \"Press return key to continue...\"");
     system("clear");
