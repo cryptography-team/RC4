a95b1f9a48d6b8b310be942552c227940b4d04e2
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-05-08 21:50:35 +0200

    included cstring and added enum SIZE to RC4

--- include/RC4.h
+++ include/RC4.h
@@ -1,35 +1,36 @@
 #ifndef RC4_H_INCLUDED
 #define RC4_H_INCLUDED
-#include <iostream>
-#include <vector>
-#include <random>
 #include <algorithm>
 #include <chrono>
+#include <iostream>
+#include <random>
+#include <vector>
 using namespace std;
-class BBSG
-{
-private :
-    int len , n , s;
-    vector<int> primeNumbers;
-    default_random_engine generator;
-    unsigned char p, q;
-public :
-    BBSG(int length ,int num);
-    void generateTwoPrimeNumbers();
-    void generateKey(unsigned char* key);
-};
-class RC4
-{
+class BBSG {
 private:
-    unsigned char *K;
-    int k_len;
+  int len, n, s;
+  vector<int> primeNumbers;
+  default_random_engine generator;
+  unsigned char p, q;
+
 public:
-    RC4(int len);
-    void setKey(unsigned char *key);
-    void generateRandomKey();
-    string encrypt(const string& plain);
-    string decrypt(const string& cipher);
-    ~RC4();
+  BBSG(int length, int num);
+  void generateTwoPrimeNumbers();
+  void generateKey(unsigned char *key);
+};
+class RC4 {
+private:
+  enum { SIZE = 256 };
+  unsigned char *K;
+  int k_len;
+
+public:
+  RC4(int len);
+  void setKey(unsigned char *key);
+  void generateRandomKey();
+  string encrypt(const string &plain);
+  string decrypt(const string &cipher);
+  ~RC4();
 };
 
 #endif // RC4_H_INCLUDED
