9bd8c4570822d05a27ca828f4ec7bf9c6f48a49e
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-05-09 00:49:59 +0200

    added a simple interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,56 @@
+#include "RC4.h"
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main()
-{
-
+int main() {
+  RC4 streamCipher(128);
+  while (true) {
+    cout << "Choose one of the following options:\n";
+    cout << "\t1- Encrypt a plain text\n";
+    cout << "\t2- Decrypt a cipher text\n";
+    cout << "\t3- Set a random key\n";
+    cout << "\t4- Set a custom key\n";
+    cout << "\t*- Exit the program\n";
+    cout << "\nYour option: " << flush;
+    int option;
+    cin >> option;
+    cin.ignore();
+    if (option == 1) {
+      cout << "Enter your plain text:\n\t" << flush;
+      string plain;
+      getline(cin, plain);
+      cout << "The corresponding cipher text is:\n\t"
+           << streamCipher.encrypt(plain) << endl;
+    } else if (option == 2) {
+      cout << "Enter your cipher text:\n\t" << flush;
+      string cipher;
+      getline(cin, cipher);
+      cout << "The corresponding plain text is:\n\t"
+           << streamCipher.decrypt(cipher) << endl;
+    } else if (option == 3) {
+      cout << "Enter your key:\n\t" << flush;
+      string key;
+      getline(cin, key);
+      streamCipher.setKey(key);
+      cout << "The key has been set\n";
+    } else if (option == 4) {
+      cout << "Enter its length: " << flush;
+      int len;
+      cin >> len;
+      cin.ignore();
+      cout << "The following key has been generated and set:\n\t"
+           << streamCipher.generateRandomKey(len) << endl;
+    } else
+      break;
+#ifdef __unix__
+    system("read -p \"Press return key to continue...\"");
+    system("clear");
+#else
+    system("pause");
+    system("cls");
+#endif
+  }
+  return 0;
 }
